3c490c9f9d4248830ddfe69de2f0fa5d43918607
dima1234ass <dima1234ass@yandex.ru>
2020-03-04 21:35:27 +0300

    Write an analog of the built-in slice method for working with arrays

---
+++ task2_4.js
@@ -0,0 +1,9 @@
+function slice(array, fn, initial) {
+    let i = 0;
+    let result = initial || array[i++];
+    for (let i = 0; i < array.length; i++) {
+        results.push(array[i]);
+    }
+    fn[a, b] = result[i]
+    return fn;
+}
